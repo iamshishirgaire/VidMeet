3969ebded36d740c29a3c6764b79f3e2c61cf3b8
iamshishirgaire <shishirgaire35@gmail.com>
2024-04-26 20:05:36 +0545

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+
 export default function HomePage() {
+  const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background text-white">
-      <h1 className="text-primary">Introduction Page</h1>
+      <h1 className=" mb-4 text-2xl font-bold">Introduction Page</h1>
+      <Button
+        onClick={() => {
+          router.push("/home");
+        }}
+      >
+        Go to dashboard
+      </Button>
     </main>
   );
 }
